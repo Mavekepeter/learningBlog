dc90dabb7a4eb8e460271a930ae59ab126379fae
samuel maveke <mavekepeter2022@gmail.com>
2024-10-19 18:41:45 +0300

    create and run server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -6,9 +5,11 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
        <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
