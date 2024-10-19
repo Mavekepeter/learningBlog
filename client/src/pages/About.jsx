8cfb718d0067a95df07e93aab50db8ed8dbb5a88
samuel maveke <mavekepeter2022@gmail.com>
2024-10-19 13:38:15 +0300

    setting up routes and structure of client

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      ll
+    </div>
+  )
+}
+
+export default About
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+      df
+    </div>
+  )
+}
+
+export default SignUp
