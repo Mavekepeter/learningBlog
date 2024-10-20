555e0d619888455ae5d758bf0a299a4eb4fb39c1
samuel maveke <mavekepeter2022@gmail.com>
2024-10-20 19:57:10 +0300

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,40 +1,41 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
-import React ,{useState}from 'react'
+import  {useState}from 'react'
 import { Link,useNavigate} from 'react-router-dom'
+import { useDispatch,useSelector } from 'react-redux'
+import {signInStart,signInSuccess,signInFailure} from '../redux/user/userslice'
 const SignIn = () => {
   const [formData, setformData] = useState({});
-  const [erroMessage, setErroMessage] = useState(null)
-  const [loading, setLoading] = useState(false)
+  const {loading,error:erroMessage} = useSelector(state =>state.user)
+  const dispatch = useDispatch()
   const navigate = useNavigate()
   const handleChange = (e)=>{
     setformData({...formData,[e.target.id]:e.target.value.trim()})
     };
-    const handleSubmit = async (e)=>{
+    const handleSubmit = async (e) => {
       e.preventDefault();
-      if ( !formData.email || !formData.password) {
-        return setErroMessage('please fill out all fields')
+      if (!formData.email || !formData.password) {
+        return dispatch(signInFailure('Please fill all the fields'));
       }
       try {
-        setLoading(true);
-        setErroMessage(null)
-        const res = await fetch('/api/auth/signin',{
-          method:"POST",
-          headers:{'Content-type':'application/json'},
-          body:JSON.stringify(formData)
+        dispatch(signInStart());
+        const res = await fetch('/api/auth/signin', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(formData),
         });
         const data = await res.json();
         if (data.success === false) {
-          return setErroMessage(data.message)
+          dispatch(signInFailure(data.message));
         }
-        setLoading(false);
-        if(res.ok){
-          navigate('/')
+  
+        if (res.ok) {
+          dispatch(signInSuccess(data));
+          navigate('/');
         }
       } catch (error) {
-        setErroMessage(error.message);
-        setLoading(false);
+        dispatch(signInFailure(error.message));
       }
-    }    
+    };
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
