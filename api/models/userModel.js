dd34bb43b3d1f6cfeafc4d66b9b5c135154e5504
samuel maveke <mavekepeter2022@gmail.com>
2024-10-23 09:09:47 +0300

    add admin functionality to the user

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -18,7 +18,11 @@ const userSchema = new mongoose.Schema({
     profilepicture:{
         type:String,
         default:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw0Eb5XABDEf7CN7ItfSG4XU&ust=1729570945197000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMjTjILQnokDFQAAAAAdAAAAABAE"
-    }
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false,
+    },
 },{timestamps:true}
 );
 const User = mongoose.model('User',userSchema);
